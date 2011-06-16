cf6b5b268110fa155a9e3c22955c53766c79b2a4
shannongamedev <shannongamedev@gmail.com>
2011-06-16 07:51:11 +0000

    Set up a BasicGameState to start the game.

---
+++ Platformer/src/platformer/Platformer.java
@@ -0,0 +1,26 @@
+package platformer;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class Platformer extends BasicGameState {
+
+	public int getID() {
+		return Main.GAME_STATE;
+	}
+	
+	public void init(GameContainer gc, StateBasedGame sb) throws SlickException {
+		
+	}
+
+	public void render(GameContainer gc, StateBasedGame sb, Graphics g) throws SlickException {
+		
+	}
+
+	public void update(GameContainer gc, StateBasedGame sb, int delta) throws SlickException {
+		
+	}
+}
