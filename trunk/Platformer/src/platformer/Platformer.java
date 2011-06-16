17d6bb81fc146458582e944f59f3b7f9e34a8858
shannongamedev@gmail.com <shannongamedev@gmail.com@30ed20f8-8a38-af85-8c36-6873829eec07>
2011-06-16 08:09:02 +0000

    Movement added, and the ability to leave the bottom added.

--- trunk/Platformer/src/platformer/Platformer.java
+++ trunk/Platformer/src/platformer/Platformer.java
@@ -2,25 +2,47 @@ package platformer;
 
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
 
 public class Platformer extends BasicGameState {
-
+	int accTime;
+	float px = 0, py = 320 - 32;
+	Image p, w;
+	Input input;
+	
 	public int getID() {
 		return Main.GAME_STATE;
 	}
 	
 	public void init(GameContainer gc, StateBasedGame sb) throws SlickException {
+		p = new Image("images/p.png");
+		//w = new Image("images/w.png");
 		
+		input = gc.getInput();
 	}
 
 	public void render(GameContainer gc, StateBasedGame sb, Graphics g) throws SlickException {
+		p.draw(px, py);
 		
 	}
 
 	public void update(GameContainer gc, StateBasedGame sb, int delta) throws SlickException {
+		accTime += delta;
 		
+		while (accTime > 0) {
+			accTime -= 20;
+			
+			if (input.isKeyDown(Input.KEY_RIGHT)) {
+				px += 4;
+			}
+		}
+		
+		if (px > 480 - 32) {
+			px = 480 - 32;
+		}
 	}
 }
